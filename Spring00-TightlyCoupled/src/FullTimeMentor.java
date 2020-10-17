c99ef7358bdefaf7309ea74dcdd99a6e83b57313
ozzy <ozzy@cybertekschool.com>
2020-10-16 22:57:33 -0400

    Spring Container

--- Spring00-TightlyCoupled/src/FullTimeMentor.java
+++
@@ -1,5 +0,0 @@
-public class FullTimeMentor {
-    public void createAccount() {
-        System.out.println("Fulltime Mentor Account is created...");
-    }
-}
--- Spring00-TightlyCoupled/src/PartTimeMentor.java
+++
@@ -1,5 +0,0 @@
-public class PartTimeMentor {
-    public void createAccount() {
-        System.out.println("Parttime Mentor Account is created...");
-    }
-}
